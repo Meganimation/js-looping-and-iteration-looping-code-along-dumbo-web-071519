883ab6c77fa0cafb91c4e98318b12e60408cf677
Megan Adams <meganoliviawendyadams@gmail.com>
2019-08-26 02:02:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ return thankYouCards;
 writeCards(["Lisa", "Kaitlin", "Jan"], thankYouCards);
 
 
+<<<<<<< HEAD
 function countdown(countdown) {
   while (countdown > 0) {
     console.log(countdown);
@@ -25,6 +26,12 @@ function countdown(countdown) {
   console.log (countdown);
   
   
+=======
+function countdown() {
+  for (let i = 0; i < 10; i--) {
+    console.log(i);
+  }
+>>>>>>> 7e07676a93854ed220cf744a4bac50b4fac21a35
   
   
 }
